067bf0b8e6866c211add11320d67b8453991e9ee
Hardik Joshi <hardikj7321@gmail.com>
2020-08-13 05:14:43 +0530

    updated sharing

--- public/js/pdfViewer.js
+++ public/js/pdfViewer.js
@@ -261,37 +261,6 @@ function viewPdf(id, courseTopic, pdfFileLocation, fileId) {
     );
   });
 }
-
-$(".social-share.facebook").on("click", function () {
-  console.log("facebook");
-  ga(
-    "send",
-    "event",
-    "COURSE_SHARED",
-    document.querySelector(".course-name").innerText,
-    "shared on facebook"
-  );
-});
-$(".social-share.twitter").on("click", function () {
-  console.log("twitter");
-  ga(
-    "send",
-    "event",
-    "COURSE_SHARED",
-    document.querySelector(".course-name").innerText,
-    "shared on twitter"
-  );
-});
-$(".social-share.email").on("click", function () {
-  console.log("gmail");
-  ga(
-    "send",
-    "event",
-    "COURSE_SHARED",
-    document.querySelector(".course-name").innerText,
-    "shared on email"
-  );
-});
 $(document).ready(() => {
   for (let i = 1; i <= 5; i++) {
     if (document.getElementById(`adobe-dc-view${i}`) != null) {
@@ -317,4 +286,57 @@ $(document).ready(() => {
       email: userEmail,
     },
   };
+  function socialWindow(url) {
+    var left = (screen.width - 570) / 2;
+    var top = (screen.height - 570) / 2;
+    var params =
+      "menubar=no,toolbar=no,status=no,width=570,height=570,top=" +
+      top +
+      ",left=" +
+      left;
+    window.open(url, "NewWindow", params);
+  }
+  function setShareLinks() {
+    var pageUrl = encodeURIComponent(document.URL);
+    var tweet = encodeURIComponent(
+      $("meta[property='og:description']").attr("content")
+    );
+    $(".social-share.facebook").on("click", function () {
+      url = "https://www.facebook.com/sharer.php?u=" + pageUrl;
+      socialWindow(url);
+      ga(
+        "send",
+        "event",
+        "COURSE_SHARED",
+        document.querySelector(".course-name").innerText,
+        "shared on facebook"
+      );
+    });
+    $(".social-share.twitter").on("click", function () {
+      url =
+        "https://twitter.com/intent/tweet?url=" + pageUrl + "&text=" + tweet;
+      socialWindow(url);
+      ga(
+        "send",
+        "event",
+        "COURSE_SHARED",
+        document.querySelector(".course-name").innerText,
+        "shared on twitter"
+      );
+    });
+    $(".social-share.email").on("click", function () {
+      url =
+        "mailto:?subject=Course%20from%20Education%20Hub&body=Hi,%20I%20found%20this%20course%20from%20the%20Education%20Hub.%20%20" +
+        pageUrl;
+      socialWindow(url);
+      ga(
+        "send",
+        "event",
+        "COURSE_SHARED",
+        document.querySelector(".course-name").innerText,
+        "shared on email"
+      );
+    });
+  }
+  setShareLinks();
 });
