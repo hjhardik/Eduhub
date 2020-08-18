53b59c2cc27bfd10d0794b67429c0a76c372aa77
Hardik Joshi <hardikj7321@gmail.com>
2020-08-18 10:48:25 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 //If want to use your own MONGODB database update the db password as follows:
 //dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
 dbPassword =
-  "mongodb+srv://hardik:hjhardik@cluster0.vsi2m.mongodb.net/<dbname>?retryWrites=true&w=majority";
+  'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
 
 //export the database URI
 module.exports = {
